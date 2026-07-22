068e648040da8a39a5b4b1513781beda6bf51ea1
Ved <vedbd@yahoo.com>
2026-07-23 00:38:10 +0530

    [ved]: half-baked vigenere practice code

---
+++ ved/implementations/basic ciphers/vigenere.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+// important: 'A' + (j + i % 26) % 26
+
+char get_encrypted_letter(int pl_index, int kl_index) {
+    // printf("pl_index: %d\tkl_index: %d\n", pl_index, kl_index);
+    return 'A' + (kl_index + pl_index % 26) % 26;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        // __FILE__ is NOT the file name. It's the source name during compilation.
+        fprintf(stdout, "USAGE: %s [key] [phrase]\n", argv[0]);
+        return 1;
+    }
+
+    int key_len = strlen(argv[1]);
+    int phrase_len = strlen(argv[2]);
+
+    char *key = argv[1];
+    char *phrase = argv[2];
+
+    int key_index = 0;
+    for (int i = 0; i < phrase_len; i++) {
+        if (!isalpha(phrase[i])) {
+            putchar(phrase[i]);
+            continue;
+        }
+        // this should be it's own check but whatever for now.
+        if (!isalpha(key[key_index % key_len])) {
+            key_index++;
+        }
+
+        putchar(get_encrypted_letter(
+            toupper(phrase[i]) - 'A',
+            toupper(key[key_index % key_len]) - 'A'
+        ));
+
+        key_index++;
+    }
+
+    return 0;
+}
