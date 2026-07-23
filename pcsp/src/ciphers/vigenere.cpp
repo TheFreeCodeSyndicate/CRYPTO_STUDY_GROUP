860a400973e3185a9c5096db8e264f68661df421
Ved <vedbd@yahoo.com>
2026-07-23 19:02:45 +0530

    [ved][fix]: bug fixes in ncrypt and decrypt functions

--- pcsp/src/ciphers/vigenere.cpp
+++ pcsp/src/ciphers/vigenere.cpp
@@ -14,9 +14,18 @@ uint8_t C(uint8_t P, uint8_t K) {
     return (P_norm + K_norm) % 26; 
 }
 
-uint8_t P(uint8_t C, uint16_t K) {
+uint8_t P(uint8_t C, uint8_t K) {
     uint8_t C_norm = index_norm(C), K_norm = index_norm(K);
     return (C_norm - K_norm + 26) % 26;
+}
+
+// even if atleast one character is alphabetic
+bool is_str_alpha(const std::vector<uint8_t>& s) {
+    if (s.empty()) return false;
+    for (uint8_t c : s) {
+        if (std::isalpha(c)) { return true; }
+    }
+    return false;
 }
 
 // DONE(REQ-CIP-501): implement VigenereCipher::encrypt
@@ -26,23 +35,27 @@ std::vector<uint8_t> VigenereCipher::encrypt(
     const std::vector<uint8_t>& key) const
 {
     // reject empty key
-    if (key.size() <= 0) {
+    if (key.size() <= 0 || !is_str_alpha(key)) {
         // not including the error message here right now.
         return {};
     }
-    std::vector<uint8_t> ciphertext(plaintext.size());
+    std::vector<uint8_t> ciphertext;
+    ciphertext.reserve(plaintext.size());
     size_t key_index = 0;
 
     for (size_t i = 0; i < plaintext.size(); i++) {
         if (! std::isalpha(plaintext[i])) {
-            ciphertext[i] = plaintext[i];
+            ciphertext.push_back(plaintext[i]);
             continue;
         }
-        if (! std::isalpha(key[key_index % key.size()])) {
+        while (! std::isalpha(key[key_index % key.size()])) {
             key_index = (key_index + 1) % key.size();
         }
+        
         bool l = plaintext[i] >= 'a';
-        ciphertext[i] = (l * 'a' + (!l) * 'A') + C(plaintext[i], key[key_index]);
+        ciphertext.push_back(
+            (l * 'a' + (!l) * 'A') + C(plaintext[i], key[key_index])
+        );
         key_index = (key_index + 1) % key.size();
     }
 
@@ -54,23 +67,27 @@ std::vector<uint8_t> VigenereCipher::decrypt(
     const std::vector<uint8_t>& ciphertext,
     const std::vector<uint8_t>& key) const
 {
-    if (key.size() <= 0) {
+    if (key.size() <= 0 || !is_str_alpha(key)) {
         // not including the error message here right now.
         return {};
     }
-    std::vector<uint8_t> plaintext(ciphertext.size());
+    std::vector<uint8_t> plaintext;
+    plaintext.reserve(ciphertext.size());
+
     size_t key_index = 0;
 
     for (size_t i = 0; i < ciphertext.size(); i++) {
         if (! std::isalpha(ciphertext[i])) {
-            plaintext[i] = ciphertext[i];
+            plaintext.push_back(ciphertext[i]);
             continue;
         }
-        if (! std::isalpha(key[key_index % key.size()])) {
+        while (! std::isalpha(key[key_index % key.size()])) {
             key_index = (key_index + 1) % key.size();
         }
         bool l = ciphertext[i] >= 'a';
-        plaintext[i] = (l * 'a' + (!l) * 'A') + P(ciphertext[i], key[key_index]);
+        plaintext.push_back(
+            (l * 'a' + (!l) * 'A') + P(ciphertext[i], key[key_index])
+        );
         key_index = (key_index + 1) % key.size();
     }
 
